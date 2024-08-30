20b0946cfc40f94191e66e7364114a009378a766
Bui Chi Vi <buichivi04062002@gmail.com>
2024-08-30 23:38:52 +0700

    UPDATE: Fetch data and create /repos api endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/repos', async (req, res) => {
-  const res = await fetch('https://api.github.com/users/freeCodeCamp/repos');
-  res.status(200).json({
-    data: res,
-  });
+  try {
+    const response = await fetch('https://api.github.com/users/freeCodeCamp/repos');
+    const data = await response.json();
+
+    res.status(200).json({
+      data: data.filter((repo) => repo.fork === false && repo.forks > 5),
+    });
+  } catch (err) {
+    res.status(404).json({ message: 'Failed to fetch data' });
+  }
 });
 
 app.listen(port, () => {
